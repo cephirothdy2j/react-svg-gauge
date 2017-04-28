229465f3ff7fa92eda81494b89c08686477955ad
Dylan Schuster <dylan@getstructure.io>
2017-04-28 10:11:14 -0400

    beginning losant changes

--- src/Gauge.js
+++ src/Gauge.js
@@ -8,9 +8,9 @@ export default class Gauge extends Component {
 		value: 40,
 		width: 400,
 		height: 320,
+		includeShadow: false,
 		color: '#fe0400',
 		backgroundColor: "#edebeb",
-		topLabelStyle: {textAnchor: "middle", fill:"#999999", stroke: "none", fontStyle: "normal",fontVariant: "normal", fontWeight: 'bold', fontStretch: 'normal', lineHeight: 'normal', fillOpacity: 1},
 		valueLabelStyle: {textAnchor: "middle", fill:"#010101", stroke: "none", fontStyle: "normal", fontVariant: "normal", fontWeight: 'bold', fontStretch: 'normal', lineHeight: 'normal', fillOpacity: 1},
 		minMaxLabelStyle: {textAnchor: "middle", fill:"#999999", stroke: "none", fontStyle: "normal",fontVariant: "normal", fontWeight: 'normal', fontStretch: 'normal', fontSize: 20, lineHeight: 'normal', fillOpacity: 1}
 	};
@@ -25,7 +25,7 @@ export default class Gauge extends Component {
 
 		var alpha = (1 - (value - this.props.min) / (this.props.max - this.props.min)) * Math.PI;
 		var Ro = this.props.width / 2 - this.props.width / 10;
-		var Ri = Ro - this.props.width / 6.666666666666667;
+		var Ri = Ro - this.props.width / 5;
 
 		var Cx = this.props.width / 2 + dx;
 		var Cy = this.props.height / 1.25 + dy;
@@ -56,30 +56,26 @@ export default class Gauge extends Component {
 	};
 
 	render() {
-		var topLabelStyle = (this.props.topLabelStyle.fontSize
-				? this.props.topLabelStyle
-				: {...this.props.topLabelStyle, fontSize: (this.props.width / 10) });
 		var valueLabelStyle = (this.props.valueLabelStyle.fontSize
 				? this.props.valueLabelStyle
 				: {...this.props.valueLabelStyle, fontSize: (this.props.width / 5) });
 		var { Cx, Ro, Ri, Xo, Cy, Xi } = this._getPathValues(this.props.max);
 		return (
 				<svg height="100%" version="1.1" width="100%" xmlns="http://www.w3.org/2000/svg" style={{width: this.props.width, height: this.props.height, overflow: 'hidden', position: 'relative', left: 0, top: 0}}>
-					<defs>
-						<filter id="g3-inner-shadow">
-							<feOffset dx="0" dy="3" />
-							<feGaussianBlur result="offset-blur" stdDeviation="5" />
-							<feComposite operator="out" in="SourceGraphic" in2="offset-blur" result="inverse" />
-							<feFlood floodColor="black" floodOpacity="0.2" result="color" />
-							<feComposite operator="in" in="color" in2="inverse" result="shadow" />
-							<feComposite operator="over" in="shadow" in2="SourceGraphic" />
-						</filter>
-					</defs>
-					<path fill={this.props.backgroundColor} stroke="none" d={this._getPath(this.props.max)} filter="url(#g3-inner-shadow)" />
-					<path fill={this.props.color} stroke="none" d={this._getPath(this.props.value)} filter="url(#g3-inner-shadow)" />
-					<text x={this.props.width / 2} y={this.props.height / 8} textAnchor="middle" style={topLabelStyle}>
-						{ this.props.label }
-					</text>
+					{this.props.includeShadow ? (
+						<defs>
+							<filter id="g3-inner-shadow">
+								<feOffset dx="0" dy="3" />
+								<feGaussianBlur result="offset-blur" stdDeviation="5" />
+								<feComposite operator="out" in="SourceGraphic" in2="offset-blur" result="inverse" />
+								<feFlood floodColor="black" floodOpacity="0.2" result="color" />
+								<feComposite operator="in" in="color" in2="inverse" result="shadow" />
+								<feComposite operator="over" in="shadow" in2="SourceGraphic" />
+							</filter>
+						</defs>
+					) : null}
+					<path fill={this.props.backgroundColor} stroke="none" d={this._getPath(this.props.max)} filter={this.props.includeShadow ? "url(#g3-inner-shadow)" : undefined} />
+					<path fill={this.props.color} stroke="none" d={this._getPath(this.props.value)} filter={this.props.includeShadow ? "url(#g3-inner-shadow)" : undefined} />
 					<text x={this.props.width / 2} y={this.props.height / 5 * 4} textAnchor="middle" style={valueLabelStyle}>
 						{ this.props.value }
 					</text>
